b4d5fbf24877ac2e1dea7fa854c3e4783506fe7e
AndreaZain <andrea.kinan@gmail.com>
2024-03-27 16:25:16 +0700

    enhance-loader-from-doitem
    add loader from doitem

--- Com.Efrata.Service.Purchasing.Lib/Facades/GarmentUnitReceiptNoteFacades/GarmentDOItemFacade.cs
+++ Com.Efrata.Service.Purchasing.Lib/Facades/GarmentUnitReceiptNoteFacades/GarmentDOItemFacade.cs
@@ -1,15 +1,20 @@
 ﻿using AutoMapper;
 using Com.Efrata.Service.Purchasing.Lib.Helpers;
+using Com.Efrata.Service.Purchasing.Lib.Helpers.ReadResponse;
 using Com.Efrata.Service.Purchasing.Lib.Interfaces;
 using Com.Efrata.Service.Purchasing.Lib.Models.GarmentPurchaseRequestModel;
 using Com.Efrata.Service.Purchasing.Lib.Models.GarmentDeliveryOrderModel;
 using Com.Efrata.Service.Purchasing.Lib.Models.GarmentExternalPurchaseOrderModel;
 using Com.Efrata.Service.Purchasing.Lib.Models.GarmentInternalPurchaseOrderModel;
 using Com.Efrata.Service.Purchasing.Lib.Models.GarmentInventoryModel;
+using Com.Efrata.Service.Purchasing.Lib.Models.GarmentPurchaseRequestModel;
 using Com.Efrata.Service.Purchasing.Lib.Models.GarmentUnitDeliveryOrderModel;
 using Com.Efrata.Service.Purchasing.Lib.Models.GarmentUnitExpenditureNoteModel;
 using Com.Efrata.Service.Purchasing.Lib.Models.GarmentUnitReceiptNoteModel;
 using Com.Efrata.Service.Purchasing.Lib.Services;
+using Com.Efrata.Service.Purchasing.Lib.ViewModels.GarmentUnitReceiptNoteViewModels;
+using Com.Moonlay.Models;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
 using System;
@@ -24,6 +29,8 @@ using System.IO;
 using Com.Efrata.Service.Purchasing.Lib.PDFTemplates.GarmentUnitReceiptNotePDFTemplates;
 using System.Data;
 using OfficeOpenXml;
+using System.Linq.Dynamic.Core;
+using System.Threading.Tasks;
 
 namespace Com.Efrata.Service.Purchasing.Lib.Facades.GarmentUnitReceiptNoteFacades
 {
@@ -31,6 +38,7 @@ namespace Com.Efrata.Service.Purchasing.Lib.Facades.GarmentUnitReceiptNoteFacade
     {
         private readonly string USER_AGENT = "Facade";
 
+        private readonly PurchasingDbContext dbContext;
         private readonly IServiceProvider serviceProvider;
         private readonly IdentityService identityService;
 
@@ -45,6 +53,7 @@ namespace Com.Efrata.Service.Purchasing.Lib.Facades.GarmentUnitReceiptNoteFacade
         private readonly DbSet<GarmentPurchaseRequest> garmentPurchaseRequests;
         private readonly PurchasingDbContext dbContext;
 
+        private readonly DbSet<GarmentPurchaseRequestItem> dbSetGarmentPRrItem;
 
         public GarmentDOItemFacade(IServiceProvider serviceProvider, PurchasingDbContext dbContext)
         {
@@ -61,11 +70,12 @@ namespace Com.Efrata.Service.Purchasing.Lib.Facades.GarmentUnitReceiptNoteFacade
             garmentUnitDeliveryOrders = dbContext.Set<GarmentUnitDeliveryOrder>();
             garmentPurchaseRequests = dbContext.Set<GarmentPurchaseRequest>();
             this.dbContext = dbContext;
+            dbSetGarmentPRrItem = dbContext.Set<GarmentPurchaseRequestItem>();
         }
 
         public List<object> ReadForUnitDO(string Keyword = null, string Filter = "{}")
         {
-            var GarmentDOItemsQuery = dbSetGarmentDOItems.Where(entity=>entity.RemainingQuantity>0).Select(a=> new { a.Id, a.EPOItemId, a.URNItemId, a.UnitId, a.StorageId, a.RO, a.POSerialNumber});
+            var GarmentDOItemsQuery = dbSetGarmentDOItems.Where(entity => entity.RemainingQuantity > 0).Select(a => new { a.Id, a.EPOItemId, a.URNItemId, a.UnitId, a.StorageId, a.RO, a.POSerialNumber });
             IQueryable<GarmentUnitReceiptNoteItem> GarmentUnitReceiptNoteItemsQuery = dbSetGarmentUnitReceiptNoteItem;
             //IQueryable<GarmentUnitReceiptNote> GarmentUnitReceiptNotesQuery = dbSetGarmentUnitReceiptNote;
             //IQueryable<GarmentExternalPurchaseOrderItem> GarmentExternalPurchaseOrderItemsQuery = dbSetGarmentExternalPurchaseOrderItem;
@@ -105,12 +115,12 @@ namespace Com.Efrata.Service.Purchasing.Lib.Facades.GarmentUnitReceiptNoteFacade
                     GarmentDOItemsQuery = GarmentDOItemsQuery.Where(x => x.POSerialNumber == POSerialNumber);
                 }
             }
-            
+
 
             List<object> ListData = new List<object>();
             var data = from doi in GarmentDOItemsQuery
                        join urni in GarmentUnitReceiptNoteItemsQuery.IgnoreQueryFilters() on doi.URNItemId equals urni.Id
-                       where  (urni.IsDeleted == true && urni.DeletedAgent=="LUCIA") || (urni.IsDeleted == false) 
+                       where (urni.IsDeleted == true && urni.DeletedAgent == "LUCIA") || (urni.IsDeleted == false)
                        select new
                        {
                            DOItemsId = doi.Id,
@@ -118,9 +128,9 @@ namespace Com.Efrata.Service.Purchasing.Lib.Facades.GarmentUnitReceiptNoteFacade
                            doi.URNItemId,
                            doi.EPOItemId,
                        };
-            var urnIds= data.Select(s => s.URNId).ToList().Distinct().ToList();
+            var urnIds = data.Select(s => s.URNId).ToList().Distinct().ToList();
             var URNs = dbSetGarmentUnitReceiptNote.IgnoreQueryFilters().Where(u => urnIds.Contains(u.Id))
-                .Select(s => new { s.Id, s.URNNo}).ToList();
+                .Select(s => new { s.Id, s.URNNo }).ToList();
             var urnItemIds = data.Select(s => s.URNItemId).ToList().Distinct().ToList();
             var urnItems = dbSetGarmentUnitReceiptNoteItem.IgnoreQueryFilters().Where(w => urnItemIds.Contains(w.Id))
                 .Select(s => new { s.Id, s.DODetailId, s.ProductRemark, s.PricePerDealUnit, s.ReceiptCorrection, s.CorrectionConversion }).ToList();
@@ -202,7 +212,7 @@ namespace Com.Efrata.Service.Purchasing.Lib.Facades.GarmentUnitReceiptNoteFacade
         public List<object> ReadForUnitDOMore(string Keyword = null, string Filter = "{}", int size = 50)
         {
             IQueryable<GarmentDOItems> GarmentDOItemsQuery = dbSetGarmentDOItems.Where(w => w.IsDeleted == false);
-            
+
             Dictionary<string, string> FilterDictionary = JsonConvert.DeserializeObject<Dictionary<string, string>>(Filter);
             long unitId = 0;
             long storageId = 0;
@@ -228,8 +238,8 @@ namespace Com.Efrata.Service.Purchasing.Lib.Facades.GarmentUnitReceiptNoteFacade
             //GarmentDOItemsQuery = GarmentDOItemsQuery.Where(x => x.RemainingQuantity > 0 && (x.RO.Contains(Keyword) || x.POSerialNumber.Contains(Keyword) || x.ProductName.Contains(Keyword) || x.ProductCode.Contains(Keyword)));
 
             var data = from doi in GarmentDOItemsQuery
-                       where doi.RemainingQuantity>0
-                       && (doi.RO.Contains(Keyword) || doi.POSerialNumber.Contains(Keyword)|| doi.ProductName.Contains(Keyword) || doi.ProductCode.Contains(Keyword))
+                       where doi.RemainingQuantity > 0
+                       && (doi.RO.Contains(Keyword) || doi.POSerialNumber.Contains(Keyword) || doi.ProductName.Contains(Keyword) || doi.ProductCode.Contains(Keyword))
                        select new
                        {
                            doi.URNItemId,
@@ -240,6 +250,7 @@ namespace Com.Efrata.Service.Purchasing.Lib.Facades.GarmentUnitReceiptNoteFacade
                            doi.RemainingQuantity,
                            DOItemsId = doi.Id,
                            doi.Colour,
+                           DOItemsId = doi.Id
                        };
 
             List<object> ListData = new List<object>(data.OrderBy(o => o.RONo).Take(size));
@@ -291,6 +302,77 @@ namespace Com.Efrata.Service.Purchasing.Lib.Facades.GarmentUnitReceiptNoteFacade
         {
             int Updated = 0;
             GarmentDOItems dataToCreate = new GarmentDOItems();
+        //
+
+        public ReadResponse<dynamic> ReadForCC(int Page = 1, int Size = 25, string Order = "{}", string Keyword = null, string Filter = "{}", string Select = null, string Search = "[]")
+        {
+            IQueryable<GarmentDOItems> Query = dbSetGarmentDOItems.Where(x => x.RemainingQuantity > 0);
+
+            List<string> SearchAttributes = JsonConvert.DeserializeObject<List<string>>(Search);
+            if (SearchAttributes.Count < 1)
+            {
+                SearchAttributes = new List<string>() { "POSerialNumber", "RO" };
+            }
+
+            IQueryable<GarmentPurchaseRequestItem> QueryPRItem = dbSetGarmentPRrItem;
+
+            //Query = QueryHelper<GarmentDOItems>.ConfigureSearch(Query, SearchAttributes, Keyword, WithAny: false);
+
+            //Dictionary<string, string> FilterDictionary = JsonConvert.DeserializeObject<Dictionary<string, string>>(Filter);
+            //Query = QueryHelper<GarmentDOItems>.ConfigureFilter(Query, FilterDictionary);
+
+            //Dictionary<string, string> OrderDictionary = JsonConvert.DeserializeObject<Dictionary<string, string>>(Order);
+            //Query = QueryHelper<GarmentDOItems>.ConfigureOrder(Query, OrderDictionary);
+
+            var QueryDOItem = (from a in Query
+                               join b in QueryPRItem on a.PRItemId equals b.Id
+                               where (string.IsNullOrWhiteSpace(Keyword) ? true : (a.POSerialNumber.Contains(Keyword) || a.RO.Contains(Keyword)))
+
+                               select new GarmentDOItemForCCViewModel
+                               {
+                                  GarmentPRId = a.Id,
+                                  GarmentPRItemId = a.PRItemId,
+                                  POSerialNumber = a.POSerialNumber,
+                                  DOItemNo = a.DOItemNo,
+                                  RONo = a.RO,
+                                  CategoryId = b.CategoryId,
+                                  CategoryCode = b.CategoryName == "FABRIC" ? "FAB" : b.ProductCode.Substring(0,3), 
+                                  CategoryName = b.CategoryName,
+                                  ProductId = a.ProductId,
+                                  ProductCode = a.ProductCode,
+                                  ProductName = a.ProductName,
+                                  ProductRemark = b.ProductRemark,
+                                  UOMId = a.SmallUomId,
+                                  UOMUnit = a.SmallUomUnit,
+                                  BudgetPrice = b.BudgetPrice,
+                                  RemainingQuantity = a.RemainingQuantity
+                                });
+
+            IQueryable SelectedQuery = QueryDOItem;
+            if (!string.IsNullOrWhiteSpace(Select))
+            {
+                SelectedQuery = QueryDOItem.Select(Select);
+            }
+
+            int totalData = SelectedQuery.Count();
+
+            if (Size > 0)
+            {
+                SelectedQuery = SelectedQuery
+                    .Skip((Page - 1) * Size)
+                    .Take(Size);
+            }
+
+            List<dynamic> Data = SelectedQuery
+                .ToDynamicList();
+
+            return new ReadResponse<dynamic>(Data, totalData, null);
+        }
+
+        public async Task<int> Patch(string id, JsonPatchDocument<GarmentDOItems> jsonPatch)
+        {
+            int Updated = 0;
+
             using (var transaction = dbContext.Database.BeginTransaction())
             {
                 try
@@ -360,6 +442,15 @@ namespace Com.Efrata.Service.Purchasing.Lib.Facades.GarmentUnitReceiptNoteFacade
                             dbSetGarmentDOItems.Add(garmentDOItems);
                             await dbContext.SaveChangesAsync();
                         }
+                    var IDs = JsonConvert.DeserializeObject<List<long>>(id);
+                    foreach (var ID in IDs)
+                    {
+                        var data = dbSetGarmentDOItems.Where(d => d.Id == ID)
+                            .Single();
+
+                        EntityExtension.FlagForUpdate(data, identityService.Username, USER_AGENT);
+
+                        jsonPatch.ApplyTo(data);
                     }
 
                     Updated = await dbContext.SaveChangesAsync();
@@ -598,6 +689,12 @@ namespace Com.Efrata.Service.Purchasing.Lib.Facades.GarmentUnitReceiptNoteFacade
             MemoryStream stream = new MemoryStream();
             package.SaveAs(stream);
             return stream;
+        }
+                    throw e;
+                }
+            }
+
+            return Updated;
         }
     }
 }
